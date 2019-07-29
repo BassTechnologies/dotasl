13dd6934ac34642245d921b1f64e3208d30a0236
bass_devware <32266684+MirchikAhtung@users.noreply.github.com>
2019-07-29 16:13:34 +0300

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -1,3 +1,3 @@
-&Ahk2Exe.exe /in "Uninstall.ahk"
-&Ahk2Exe.exe /in "ScriptUpdater.ahk"
-&Ahk2Exe.exe /in "Startup.ahk"
+"&Ahk2Exe.exe" /in "Uninstall.ahk"
+"&Ahk2Exe.exe" /in "ScriptUpdater.ahk"
+"&Ahk2Exe.exe" /in "Startup.ahk"
