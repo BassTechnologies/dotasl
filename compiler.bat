163309a48061fe2ac6a992e025368b244c2e02ac
bass_devware <32266684+MirchikAhtung@users.noreply.github.com>
2019-07-30 12:12:01 +0300

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -1,6 +1,8 @@
 @echo off
 echo %time%
+echo File Compilation: UnInstall.ahk ScriptUpdater.ahk Startup.ahk
 "&Ahk2Exe.exe" /in "Uninstall.ahk"
 "&Ahk2Exe.exe" /in "ScriptUpdater.ahk"
 "&Ahk2Exe.exe" /in "Startup.ahk"
-timeout 5 > NUL
+echo Compilation complete.
+timeout 1 > NUL
